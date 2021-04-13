80e70e1e7813d2846f32f33d91dfc6ce1a1dc734
Luna Ryuko Zaremba <contact@lunaryuko.space>
2021-04-13 23:55:12 +0200

    Move disaster engine project to its own directory. Add simple unit tests.

---
+++ tests/DisasterTests/AssetTest.cs
@@ -0,0 +1,35 @@
+using Disaster;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DisasterTests
+{
+    [TestClass]
+    public class AssetTest
+    {
+        [TestMethod]
+        public void AssetPathTest()
+        {
+            DisasterTests.Init();
+
+            Disaster.Assets.basePath = "C:/disaster5/";
+            string AssetPath = Disaster.Assets.LoadPath("test_asset.obj");
+
+            Assert.IsTrue(AssetPath == "C:/disaster5/test_asset.obj", "The asset path is not correct");
+            
+            DisasterTests.Quit();
+        }
+
+        // LUNA: Engine should be able to recover from this
+        [TestMethod]
+        public void LoadNonexistentTexture()
+        {
+            DisasterTests.Init();
+            
+            Disaster.Assets.basePath = "C:/disaster5/";
+
+            Disaster.Assets.Texture("Non-existent.png");
+            
+            DisasterTests.Quit();
+        }
+    }
+}
